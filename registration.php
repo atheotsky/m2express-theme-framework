183aa8d284a36399b40e3e6e2c4ecae2755510d0
Thanh, Nguyen Duy <nd.thanh@outlook.com>
2018-09-21 10:52:13 +0700

    change title

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/M2express/BaseTheme',
+    'frontend/M2express/Framework',
     __DIR__
 );
